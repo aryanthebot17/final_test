5b7d97bab723c994ec5b623fa9713d5dd7b2f030
aryanthebot17 <96947052+aryanthebot17@users.noreply.github.com>
2025-09-01 11:19:06 +0530

    Update ProductServiceApplication.java

--- src/main/java/com/sutherland/finaltest/ProductServiceApplication.java
+++ src/main/java/com/sutherland/finaltest/ProductServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+    // this wil be our main application for running the spring boot application
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
