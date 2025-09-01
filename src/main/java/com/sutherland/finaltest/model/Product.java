ac25aaceab6a401d83e23f94b3358fc906769ad4
aryanthebot17 <96947052+aryanthebot17@users.noreply.github.com>
2025-09-01 11:18:30 +0530

    Update Product.java

--- src/main/java/com/sutherland/finaltest/model/Product.java
+++ src/main/java/com/sutherland/finaltest/model/Product.java
@@ -1,5 +1,49 @@
 package com.sutherland.finaltest.model;
 
 public class Product {
+    private int productId;
+    private String productName;
+    private String productType;
+    private double price;
 
+    // Constructor
+    public Product(int productId, String productName, String productType, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productType = productType;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
